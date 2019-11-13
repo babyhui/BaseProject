2cea0df867d71d65247c4425ae8d0cdf8eab39e3
Eria <c121680118@gmail.com>
2019-11-13 20:05:50 +0800

    de

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
-
+#ifndef TEST
 int main() {
     printf("Rule Breaker!!!\n");
     printf("5 + 2 = %d\n", addNumber(5, 2));
     printf("%d\n", getTriangleType(3, 4, 5));
     return 0;
 }
-
+#endif // TEST
 
 int addNumber(int a, int b) {
     return a + b;
