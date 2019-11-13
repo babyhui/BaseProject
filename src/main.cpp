2c0c8264cb7be13ecde0377c7e5bfc564292b458
wennnn <umin3177@mail.fcu.edu.tw>
2019-11-13 18:56:38 +0800

    pulu

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main() {
     printf("%d\n", getTriangleType(3, 4, 5));
     return 0;
 }
-#endif // TEST123 test456
+#endif // TEST123 test456 pulu 
 
 int addNumber(int a, int b) {
     return a + b;
